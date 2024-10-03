e403f6d2d3559857e20286a451aa827650b50b20
rommel dalisay <rommelmdalisay@gmail.com>
2024-10-03 08:40:47 +0800

    add album listeners

--- listener/storeAlbums.php
+++ listener/storeAlbums.php
@@ -1,13 +1,20 @@
 <?php
 include("../includes/header.php");
 include("../includes/config.php");
-var_dump($_POST);
-var_dump($_SESSION);
+
 $album_ids = $_POST['albums'];
-foreach ($album_ids as $album_id) {
-    $sql1 = "INSERT INTO albums_listeners (listeners_listener_id, albums_album_id) VALUES({$_SESSION['listener_id']}, $album_id )";
-    $result = mysqli_query($conn, $sql1);
-   
+if(isset($_POST['albums'])) {
+    $result = mysqli_query($conn, "DELETE FROM albums_listeners WHERE  listeners_listener_id = {$_SESSION['listener_id']}");
+    foreach ($album_ids as $album_id) {
+        $sql1 = "INSERT INTO albums_listeners (listeners_listener_id, albums_album_id) VALUES({$_SESSION['listener_id']}, $album_id )";
+        $result = mysqli_query($conn, $sql1);
+       
+    }
+}
+
+
+else {
+    $result = mysqli_query($conn, "DELETE FROM albums_listeners WHERE  listeners_listener_id = {$_SESSION['listener_id']}"); 
 }
  if(mysqli_affected_rows($conn) > 0) {
 header("Location: albumList.php");
